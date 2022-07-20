22e0d7904d7e35f986c4193a522b6d0d39b7609d
achintha98 <kalanaachintha@gmail.com>
2022-07-20 22:53:56 +0530

    completed fraud service

--- customer/src/main/java/com/amgcode/customer/CustomerController.java
+++ customer/src/main/java/com/amgcode/customer/CustomerController.java
@@ -1,16 +1,22 @@
 package com.amgcode.customer;
 
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@AllArgsConstructor
 @RequestMapping("api/v1/customer")
-public record CustomerController(CustomerService customerService) {
+public class CustomerController {
 
+    private final CustomerService customerService;
+
+    @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("new customer registration{}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
---
+++ fraud/src/main/java/com/amgcode/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.amgcode.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
